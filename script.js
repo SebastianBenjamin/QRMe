145f477ca98d74a5495de90c0aada3469d954cf1
Benjamin Sebastian <142651700+SebastianBenjamin@users.noreply.github.com>
2024-09-07 01:20:51 +0530

    update

--- script.js
+++ script.js
@@ -14,8 +14,6 @@ function makeCode () {
   document.getElementById('qrimage').style.display='none';
   document.getElementById('span').style.display='none';
   qrcode.makeCode(elText.value);
-  elText.value='';
-
 }
 
 
